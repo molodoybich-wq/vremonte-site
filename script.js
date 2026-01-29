5a0d284f2baa67a983b20fbdffbdc130c86f83e0
molodoybich-wq <molodoybich@gmail.com>
2026-01-30 01:14:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+document.getElementById('leadForm').addEventListener('submit', e => {
+  e.preventDefault();
 
+  const name = name.value;
+  const phone = phone.value;
+  const problem = document.getElementById('problem').value;
+
+  const text =
+    `Заявка с сайта (курьер бесплатно)\n` +
+    `Имя: ${name}\n` +
+    `Телефон: ${phone}\n` +
+    `Проблема: ${problem}`;
+
+  window.open(
+    'https://t.me/share/url?text=' + encodeURIComponent(text),
+    '_blank'
+  );
+});
